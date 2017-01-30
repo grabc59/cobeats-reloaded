98186046a85cf5c85252232fc0449104b9da1e56
Christopher Grabski <grabc59@gmail.com>
2017-01-29 22:54:43 -0700

    clean up comments

--- src/client/js/main/tone-sequencer/tone-sequencer.component.js
+++ src/client/js/main/tone-sequencer/tone-sequencer.component.js
@@ -26,16 +26,6 @@
     //////////////////////////////////
     function postLink() {
       console.log("post link fired");
-      // try {
-      //   WebFont.load({
-      //     google: {
-      //       families: ['Open Sans']
-      //     }
-      //   });
-      // } catch(e) {
-      //   console.log("font not loaded")
-      // }
-
       nx.addStylesheet();
       // console.log('window onload, this is where nx starts its functions')
       // get all canvases on the page and add them to the manager
@@ -47,59 +37,8 @@
         document.addEventListener("touchmove", nx.blockMove, true);
         document.addEventListener("touchstart", nx.blockMove, true);
       }
-
-  // nx.onload();
-
-  // nx.startPulse();
-
-
-
-      // nx.addStylesheet();
       vm.onload();
       nx.startPulse();
-      
-      // nx.onload = function() {
-      //       nx.colorize("#f5871f");
-
-      //       matrix1.col = 16;
-      //       matrix1.init();
-      //       matrix1.resize($("#Content").width(), 250);
-      //       matrix1.draw();
-      //       console.log(matrix1)
-      //       console.log("matrix1")
-      //   }
-
-
-        // $(function() {
-        //     console.log("interface loader function initialized")
-        //     Interface.Slider({
-        //         name: "BPM",
-        //         min: 80,
-        //         max: 200,
-        //         value: Tone.Transport.bpm.value,
-        //         drag: function(val) {
-        //             Tone.Transport.bpm.value = val;
-        //         }
-        //     });
-        //     Interface.Button({
-        //         text: "Start",
-        //         activeText: "Stop",
-        //         type: "toggle",
-        //         key: 32, //spacebar
-        //         start: function() {
-        //             loop.start();
-        //         },
-        //         end: function() {
-        //             loop.stop();
-        //         },
-        //     });
-        //     Interface.Loader();
-        //     $(window).on("resize", function() {
-        //         matrix1.resize($("#Content").width(), 250);
-        //         matrix1.draw();
-        //     });
-        //     console.log("initialize tone sequencer")
-        // });
     }
     vm.onload = function() {
       console.log('nx onload fired')
