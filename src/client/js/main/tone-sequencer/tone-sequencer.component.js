f36edb2f568a2cd6a12e83c314645045ae02a9d5
Christopher Grabski <grabc59@gmail.com>
2017-01-29 18:33:21 -0700

    fix toneSequencer component name

--- src/client/js/main/tone-sequencer/tone-sequencer.component.js
+++ src/client/js/main/tone-sequencer/tone-sequencer.component.js
@@ -1,7 +1,7 @@
 ( function() {
   'use strict';
   angular.module( 'app' )
-    .component( 'tone-sequencer', {
+    .component( 'toneSequencer', {
       templateUrl: "js/main/tone-sequencer/tone-sequencer.template.html",
       controller: controller
     });
